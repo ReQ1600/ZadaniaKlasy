ea815a94feb815c5cb9dbc644c25c514f75be599
Mateusz Drogowski <drogowskimateusz8@gmail.com>
2020-09-07 20:17:42 +0200

    Add project files.

---
+++ ZadaniaKlasy/Eployee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    class Eployee
+    {
+
+        string name;
+        string surname;
+        double hourlyRate;
+        Double payment = 0;
+        public double Payment { get; set; }
+        public Eployee(string name="Nie podano", string surname="Nie podano", double hourlyRate=0.0)
+        {
+            this.name = name;   this.surname = surname; this.hourlyRate = hourlyRate;
+        }
+
+        public void RegisterTime(double hours)
+        {
+            if (hours <= 8&&hours>0)
+            {
+                payment = hours*hourlyRate;
+            }
+            else if (hours>8)
+            {
+                payment = 8 * 100 + (hours-8)*200;
+            }
+            else
+            {
+                payment = 0;
+            }
+        }
+
+        public void PaySalary()
+        {
+            Console.WriteLine($"Stan należności po stronie pracodawcy={payment}");
+        }
+    }
+}
---
+++ ZadaniaKlasy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Eployee emp1 = new Eployee("Jan", "Kowalski", 100.0);
+            emp1.RegisterTime(10);
+            emp1.PaySalary();
+
+            Console.ReadKey();
+        }
+    }
+}
