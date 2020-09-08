6837c35a7c71636a4ae2806033ac9d62461c4e63
Mateusz Drogowski <drogowskimateusz8@gmail.com>
2020-09-08 21:13:52 +0200

    a

--- Zad2/PeselValidationResult.cs
+++ Zad2/PeselValidationResult.cs
@@ -23,24 +23,83 @@ namespace Zad2
             peselArray = pesel.ToCharArray();
         }
 
-        private string DateOfBirth()
+        public string DateOfBirth()
         {
-            int month = peselArray[2] * 10 + peselArray[3];
-            string sMonth=null;
+            int month=0;
+            int yearFirstNumbers;
+            if (Convert.ToInt32(peselArray[2].ToString()) - 8 >= 0)
+            {
+                month = (Convert.ToInt32(peselArray[2].ToString()) - 8) * 10 + Convert.ToInt32(peselArray[3].ToString());
+                yearFirstNumbers = 18;
+            }
+            else if (Convert.ToInt32(peselArray[2].ToString()) - 6 >= 0)
+            {
+                month = (Convert.ToInt32(peselArray[2].ToString()) - 6) * 10 + Convert.ToInt32(peselArray[3].ToString());
+                yearFirstNumbers = 22;
+            }
+            else if ((Convert.ToInt32(peselArray[2].ToString()) - 4 >= 0))
+            {
+                month = (Convert.ToInt32(peselArray[2].ToString()) - 4) * 10 + Convert.ToInt32(peselArray[3].ToString());
+                yearFirstNumbers = 21;
+            }
+            else if ((Convert.ToInt32(peselArray[2].ToString()) - 2 >= 0))
+            {
+                month = (Convert.ToInt32(peselArray[2].ToString()) - 2) * 10 + Convert.ToInt32(peselArray[3].ToString());
+                yearFirstNumbers = 20;
+            }
+            else
+            {
+                month = Convert.ToInt32(peselArray[2].ToString()) * 10 + Convert.ToInt32(peselArray[3].ToString());
+                yearFirstNumbers = 19;
+
+            }
+            string sMonth = null;
             switch (month)
             {
                 case 1:
                     sMonth = "Styczeń";
                     break;
-                default:
+                case 2:
+                    sMonth = "Luty";
+                        break;
+                case 3:
+                    sMonth = "Marzec";
+                    break;
+                case 4:
+                    sMonth = "Kwiecień";
+                    break;
+                case 5:
+                    sMonth = "Maj";
+                    break;
+                case 6:
+                    sMonth = "Czerwiec";
+                    break;
+                case 7:
+                    sMonth = "Lipiec";
+                    break;
+                case 8:
+                    sMonth = "Sierpień";
+                    break;
+                case 9:
+                    sMonth = "Wrzesień";
+                    break;
+                case 10:
+                    sMonth = "Październik";
+                    break;
+                case 11:
+                    sMonth = "Listopad";
+                    break;
+                case 12:
+                    sMonth = "Grudnień";
                     break;
             }
-            return sMonth;
+
+            return $"{yearFirstNumbers}{peselArray[0]}{peselArray[1]} {sMonth}";
         }
         public void output()
         {
-            string s = DateOfBirth();
-            Console.WriteLine(s);
+            dateOfBirth = DateOfBirth();
+            Console.WriteLine(dateOfBirth);
         }
 
     }
--- Zad2/Program.cs
+++ Zad2/Program.cs
@@ -7,6 +7,7 @@ namespace Zad2
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("podaj pesel");
