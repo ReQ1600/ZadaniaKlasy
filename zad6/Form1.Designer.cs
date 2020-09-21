82469db57e0ee962141012d6e6a6bdd2494a74c6
Mateusz Drogowski <drogowskimateusz8@gmail.com>
2020-09-21 21:37:08 +0200

    Finished

--- zad6/Form1.Designer.cs
+++ zad6/Form1.Designer.cs
@@ -39,6 +39,10 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.pb = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
             this.SuspendLayout();
             // 
@@ -79,7 +83,7 @@
             this.lbl.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.lbl.BackColor = System.Drawing.SystemColors.Control;
             this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lbl.Location = new System.Drawing.Point(139, 190);
+            this.lbl.Location = new System.Drawing.Point(137, 186);
             this.lbl.Name = "lbl";
             this.lbl.Size = new System.Drawing.Size(590, 32);
             this.lbl.TabIndex = 9;
@@ -189,20 +193,78 @@
             this.pb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pb.BackgroundImage = global::zad6.Properties.Resources.rezystor;
+            this.pb.BackColor = System.Drawing.SystemColors.Desktop;
+            this.pb.BackgroundImage = global::zad6.Properties.Resources.rezystor1;
             this.pb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pb.Location = new System.Drawing.Point(158, 12);
             this.pb.Name = "pb";
-            this.pb.Size = new System.Drawing.Size(590, 152);
+            this.pb.Size = new System.Drawing.Size(586, 148);
             this.pb.TabIndex = 7;
             this.pb.TabStop = false;
+            this.pb.Click += new System.EventHandler(this.Pb_Click);
+            this.pb.MouseLeave += new System.EventHandler(this.Pb_MouseLeave);
+            this.pb.MouseHover += new System.EventHandler(this.Pb_MouseHover);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel1.Location = new System.Drawing.Point(289, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(18, 114);
+            this.panel1.TabIndex = 17;
+            this.panel1.MouseLeave += new System.EventHandler(this.Pb_MouseLeave);
+            this.panel1.MouseHover += new System.EventHandler(this.Pb_MouseHover);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.panel2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel2.Location = new System.Drawing.Point(360, 43);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(18, 88);
+            this.panel2.TabIndex = 18;
+            this.panel2.MouseLeave += new System.EventHandler(this.Pb_MouseLeave);
+            this.panel2.MouseHover += new System.EventHandler(this.Pb_MouseHover);
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.panel3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel3.Location = new System.Drawing.Point(431, 43);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(18, 88);
+            this.panel3.TabIndex = 19;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel3_Paint);
+            this.panel3.MouseLeave += new System.EventHandler(this.Pb_MouseLeave);
+            this.panel3.MouseHover += new System.EventHandler(this.Pb_MouseHover);
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.panel4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel4.Location = new System.Drawing.Point(519, 43);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(18, 88);
+            this.panel4.TabIndex = 20;
+            this.panel4.MouseLeave += new System.EventHandler(this.Pb_MouseLeave);
+            this.panel4.MouseHover += new System.EventHandler(this.Pb_MouseHover);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 270);
+            this.ClientSize = new System.Drawing.Size(796, 266);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -211,12 +273,15 @@
             this.Controls.Add(this.cb2);
             this.Controls.Add(this.lbl);
             this.Controls.Add(this.btnCount);
-            this.Controls.Add(this.pb);
             this.Controls.Add(this.cb1);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.pb);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 308);
             this.MinimumSize = new System.Drawing.Size(816, 308);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Kalkulator Rezystancji";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
             this.ResumeLayout(false);
@@ -236,6 +301,10 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox pb;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
     }
 }
 
--- zad6/Form1.cs
+++ zad6/Form1.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace zad6
@@ -23,6 +23,10 @@ namespace zad6
         public Form1()
         {
             InitializeComponent();
+            panel1.BackColor = Color.FromArgb(225, 229, 234);
+            panel2.BackColor = Color.FromArgb(225, 229, 234);
+            panel3.BackColor = Color.FromArgb(225, 229, 234);
+            panel4.BackColor = Color.FromArgb(225, 229, 234);
             lbl.Text = "";
             f = false;
             s = false;
@@ -38,33 +42,43 @@ namespace zad6
             {
                 case 0:
                     first = null;
+                    panel1.BackColor = Color.Black;
                     break;
                 case 1:
                     first = 1;
+                    panel1.BackColor = Color.SaddleBrown;
                     break;
                 case 2:
                     first = 2;
+                    panel1.BackColor = Color.Red;
                     break;
                 case 3:
                     first = 3;
+                    panel1.BackColor = Color.DarkOrange;
                     break;
                 case 4:
                     first = 4;
+                    panel1.BackColor = Color.Yellow;
                     break;
                 case 5:
                     first = 5;
+                    panel1.BackColor = Color.Green;
                     break;
                 case 6:
                     first = 6;
+                    panel1.BackColor = Color.Blue;
                     break;
                 case 7:
                     first = 7;
+                    panel1.BackColor = Color.DarkViolet;
                     break;
                 case 8:
                     first = 8;
+                    panel1.BackColor = Color.Gray;
                     break;
                 case 9:
                     first = 9;
+                    panel1.BackColor = Color.White;
                     break;
             }
         }
@@ -75,33 +89,43 @@ namespace zad6
             {
                 case 0:
                     second = 0;
+                    panel2.BackColor = Color.Black;
                     break;
                 case 1:
                     second = 1;
+                    panel2.BackColor = Color.SaddleBrown;
                     break;
                 case 2:
                     second = 2;
+                    panel2.BackColor = Color.Red;
                     break;
                 case 3:
                     second = 3;
+                    panel2.BackColor = Color.DarkOrange;
                     break;
                 case 4:
                     second = 4;
+                    panel2.BackColor = Color.Yellow;
                     break;
                 case 5:
                     second = 5;
+                    panel2.BackColor = Color.Green;
                     break;
                 case 6:
                     second = 6;
+                    panel2.BackColor = Color.Blue;
                     break;
                 case 7:
                     second = 7;
+                    panel2.BackColor = Color.DarkViolet;
                     break;
                 case 8:
                     second = 8;
+                    panel2.BackColor = Color.Gray;
                     break;
                 case 9:
                     second = 9;
+                    panel2.BackColor = Color.White;
                     break;
             }
         }
@@ -113,33 +137,43 @@ namespace zad6
             {
                 case 0:
                     third = 0.001;
+                    panel3.BackColor = Color.Black;
                     break;
                 case 1:
                     third = 0.01;
+                    panel3.BackColor = Color.SaddleBrown;
                     break;
                 case 2:
                     third = 0.1;
+                    panel3.BackColor = Color.Red;
                     break;
                 case 3:
                     third = 1;
+                    panel3.BackColor = Color.DarkOrange;
                     break;
                 case 4:
                     third = 10;
+                    panel3.BackColor = Color.Yellow;
                     break;
                 case 5:
                     third = 100;
+                    panel3.BackColor = Color.Green;
                     break;
                 case 6:
                     third = 1000;
+                    panel3.BackColor = Color.Blue;
                     break;
                 case 7:
                     third = 10000;
+                    panel3.BackColor = Color.DarkViolet;
                     break;
                 case 8:
                     third = 0.0001;
+                    panel3.BackColor = Color.Goldenrod;
                     break;
                 case 9:
                     third = 0.00001;
+                    panel3.BackColor = Color.Silver;
                     break;
             }
         }
@@ -150,27 +184,35 @@ namespace zad6
             {
                 case 0:
                     fourth = "\u00B1 1%";
+                    panel4.BackColor = Color.SaddleBrown;
                     break;
                 case 1:
                     fourth = "\u00B1 2%";
+                    panel4.BackColor = Color.Red;
                     break;
                 case 2:
                     fourth = "\u00B1 0.5%";
+                    panel4.BackColor = Color.Green;
                     break;
                 case 3:
                     fourth = "\u00B1 0.25%";
+                    panel4.BackColor = Color.Blue;
                     break;
                 case 4:
                     fourth = "\u00B1 0.10%";
+                    panel4.BackColor = Color.DarkViolet;
                     break;
                 case 5:
                     fourth = "\u00B1 0.05%";
+                    panel4.BackColor = Color.Gray;
                     break;
                 case 6:
                     fourth = "\u00B1 5%";
+                    panel4.BackColor = Color.Goldenrod;
                     break;
                 case 7:
                     fourth = "\u00B1 10%";
+                    panel4.BackColor = Color.Silver;
                     break;
             }
         }
@@ -198,6 +240,23 @@ namespace zad6
             }
         }
 
+        private void Panel3_Paint(object sender, PaintEventArgs e)
+        {
 
+        }
+
+        private void Pb_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://pl.mouser.com/technical-resources/conversion-calculators/resistor-color-code-calculator");
+        }
+
+        private void Pb_MouseHover(object sender, EventArgs e)
+        {
+            lbl.Text = "Strona z której został \"zapożyczony\" obraz";
+        }
+
+        private void Pb_MouseLeave(object sender, EventArgs e)
+        {
+        }
     }
 }
